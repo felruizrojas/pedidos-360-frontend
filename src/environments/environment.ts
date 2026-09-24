39ea23a153988c1cdf2904f402749198b81d1365
felruizrojas <fe.ruizr@duocuc.cl>
2026-09-24 00:25:12 -0300

    feat:agregar api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   tenantId: '0bfad962-b91d-465a-b769-8e71565efe7d',
   spaClientId: '72258f06-7c3b-4189-8f22-a0abdc893ae2',
   apiClientId: 'd80d5009-1b8a-49b2-a3da-08a8bbd00936',
-  apiBaseUrl: 'https://u8thxu2opa.execute-api.us-east-1.amazonaws.com',
+  apiBaseUrl: 'https://djyz5il0mk.execute-api.us-east-1.amazonaws.com',
   redirectUri: 'https://pedidos360-felipe.vercel.app/auth-redirect',
   postLogoutRedirectUri: 'https://pedidos360-felipe.vercel.app',
   forzarAdminTemporal: true,
